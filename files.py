14f5b133fc7cd1f9f40391c8866584e18e1c15be
Varsik <varsikgrigoryan@gmail.com>
2023-01-24 20:32:32 +0400

    my first commit

---
+++ files.py
@@ -0,0 +1,48 @@
+# file = open("text.txt")
+# print(file.read())
+
+# file = open("test/new.txt")
+# print(file.read())
+
+# file = open("test/new.txt", "a")
+# file.write("something")
+
+# file = open("text.txt", "r")
+# print(file.read())
+
+# file = open("text.txt", "r")
+# print(file.read(5))
+
+# file = open("text.txt")
+# print(file.readline())
+# print(file.readline())
+
+# file = open("text.txt")
+# print(file.readlines())
+
+# with open("text.txt", "a") as file:
+# 	file.write("hello world !")
+
+# with open("text.txt", "a+") as file:
+# 	file.write("hello world !")
+# 	print(f.tell()) asuma vortexa kursor@
+# 	file.seek(0)      es tox@ kursor@ beruma zroyi vra, vor karana karda
+# 	print(file.read())
+
+
+"""6-rd xndir"""
+# import json
+# with open("text.json") as file:
+# 	x = json.load(file)
+# 	print(x)
+
+# import json
+# with open("text.json") as file:
+# 	x = json.load(file)
+# 	print(type(x[0]))
+
+# import json
+# with open("text.json") as file:
+# 	x = json.load(file)
+# 	y = json.dumps(x)
+# 	print(type(y))
